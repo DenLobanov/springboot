c192b17b23724a7a5b06b819ed6684663b589d59
DenLobanov <lobanovde2012@gmail.com>
2020-09-18 15:56:05 +0300

    add value url

--- src/main/java/com/springboot/rest/config/DatabaseConfig.java
+++ src/main/java/com/springboot/rest/config/DatabaseConfig.java
@@ -1,5 +1,7 @@
 package com.springboot.rest.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +10,15 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    String url;
+
     @Bean
     public DataSource dataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.sqlite.JDBC");
-        dataSourceBuilder.url("jdbc:sqlite:data.sqlite");
+        dataSourceBuilder.url(url);
         return dataSourceBuilder.build();
     }
 }
